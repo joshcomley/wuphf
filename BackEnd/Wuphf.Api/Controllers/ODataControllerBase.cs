c0804dcd95159efd887dbab0df121103758e61f2
Josh Comley <josh@awsm.tech>
2022-08-18 11:01:57 +0100

    Tidy

---
+++ BackEnd/Wuphf.Api/Controllers/ODataControllerBase.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData.Query;
+using Microsoft.AspNetCore.OData.Routing.Controllers;
+using Wuphf.Data;
+
+namespace Wuphf.Api.Controllers;
+
+public class ODataControllerBase<T> : ODataController 
+    where T : class
+{
+    private AppDbContext? _db;
+
+    protected AppDbContext Db => _db ??= HttpContext.RequestServices.GetRequiredService<AppDbContext>();
+
+    [HttpGet]
+    [EnableQuery]
+    public IActionResult Get(CancellationToken token)
+    {
+        return Ok(Db.Set<T>());
+    }
+}
--- BackEnd/Wuphf.Api/Controllers/ServersController.cs
+++ BackEnd/Wuphf.Api/Controllers/ServersController.cs
@@ -1,26 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.OData.Query;
-using Microsoft.AspNetCore.OData.Routing.Controllers;
-using Wuphf.Data.Models;
+﻿using Wuphf.Data.Models;
 
 namespace Wuphf.Api.Controllers;
 
 public class ServersController : ODataControllerBase<Server>
-{
-    
-}
-
-public class ODataControllerBase<T> : ODataController
-{
-    [HttpGet]
-    [EnableQuery]
-    public IActionResult Get(CancellationToken token)
-    {
-        throw new NotImplementedException();
-        // return Ok(_context.Products);
-    }
-}
-
-public record struct Server1
 {
 }
