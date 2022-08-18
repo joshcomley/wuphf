b751af6ec341368c4441daabf3ac24fa540ea31b
Tommy Lo <tommy@awsm.tech>
2022-08-18 10:08:23 +0100

    Add Server model

---
+++ BackEnd/Wuphf.Data/Models/Server.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Wuphf.Data.Models;
+
+public class Server
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public DateTimeOffset? LastAcquired { get; set; }
+}
