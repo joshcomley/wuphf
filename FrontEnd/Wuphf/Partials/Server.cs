051dbdb8142ffc83d94afefcdc81eed361bc6c12
Aliya Aziz <aliya.aziz@wedo-digital.co.uk>
2022-08-18 19:08:08 +0100

    clean up

--- FrontEnd/Wuphf/Partials/Server.cs
+++ FrontEnd/Wuphf/Partials/Server.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Globalization;
+﻿using System.Globalization;
 
 namespace Wuphf.Api.Client
 {
@@ -7,7 +6,6 @@ namespace Wuphf.Api.Client
     {
         public bool IsTaken => !IsAvailable;
         public bool IsAvailable => string.IsNullOrEmpty(UserNameLastAcquired);
-        public string BgColor => !IsAvailable && UserNameLastAcquired.Equals("Aliya") ? "#76E109" : "White";
         public Server Self => this;
     }
 
