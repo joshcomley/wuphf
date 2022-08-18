b08dd9243702dd579efe93b72f6bbbae81bfa8ba
Tommy Lo <tommy@awsm.tech>
2022-08-18 10:17:45 +0100

    Add other project files

--- Wuphf.Backend/Wuphf.Data/Models/Server.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Wuphf.Data.Models;
-
-public class Server
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public DateTimeOffset? LastAcquired { get; set; }
-}
